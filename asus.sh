549eb3b86bc99751fbf7ef23319ded2ef574d88f
Hossein <KeshavarzHossein1999@gmail.com>
2022-01-11 17:36:36 +0330

    Purge

--- asus.sh
+++ asus.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# Replace PASSWD with your actual root password :)!
-
 if [ $1 == "info" ]
 then
 	echo "CPU:"
@@ -25,28 +23,28 @@ then
 
 elif [ $1 == "limit" ]
 then
-	echo PASSWD | sudo -S sh -c "echo 80 > /sys/class/power_supply/BAT0/charge_control_end_threshold"
+	sudo -S sh -c "echo $2 > /sys/class/power_supply/BAT0/charge_control_end_threshold"
 
 elif [ $1 == "power" ]
 then
-	echo PASSWD | sudo -S sh -c "echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo"
-	echo PASSWD | sudo -S sh -c "echo power > /sys/devices/system/cpu/cpu0/cpufreq/energy_performance_preference"
-	echo PASSWD | sudo -S sh -c "echo power > /sys/devices/system/cpu/cpu1/cpufreq/energy_performance_preference"
-	echo PASSWD | sudo -S sh -c "echo power > /sys/devices/system/cpu/cpu2/cpufreq/energy_performance_preference"
-	echo PASSWD | sudo -S sh -c "echo power > /sys/devices/system/cpu/cpu3/cpufreq/energy_performance_preference"
+	sudo -S sh -c "echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo"
+	sudo -S sh -c "echo power > /sys/devices/system/cpu/cpu0/cpufreq/energy_performance_preference"
+	sudo -S sh -c "echo power > /sys/devices/system/cpu/cpu1/cpufreq/energy_performance_preference"
+	sudo -S sh -c "echo power > /sys/devices/system/cpu/cpu2/cpufreq/energy_performance_preference"
+	sudo -S sh -c "echo power > /sys/devices/system/cpu/cpu3/cpufreq/energy_performance_preference"
 
 elif [ $1 == "balance" ]
 then
-	echo PASSWD | sudo -S sh -c "echo balance_power > /sys/devices/system/cpu/cpu0/cpufreq/energy_performance_preference"
-	echo PASSWD | sudo -S sh -c "echo balance_power > /sys/devices/system/cpu/cpu1/cpufreq/energy_performance_preference"
-	echo PASSWD | sudo -S sh -c "echo balance_power > /sys/devices/system/cpu/cpu2/cpufreq/energy_performance_preference"
-	echo PASSWD | sudo -S sh -c "echo balance_power > /sys/devices/system/cpu/cpu3/cpufreq/energy_performance_preference"
+	sudo -S sh -c "echo balance_power > /sys/devices/system/cpu/cpu0/cpufreq/energy_performance_preference"
+	sudo -S sh -c "echo balance_power > /sys/devices/system/cpu/cpu1/cpufreq/energy_performance_preference"
+	sudo -S sh -c "echo balance_power > /sys/devices/system/cpu/cpu2/cpufreq/energy_performance_preference"
+	sudo -S sh -c "echo balance_power > /sys/devices/system/cpu/cpu3/cpufreq/energy_performance_preference"
 
 elif [ $1 == "performance" ]
 then
-	echo PASSWD | sudo -S sh -c "echo 0 > /sys/devices/system/cpu/intel_pstate/no_turbo"	
-	echo PASSWD | sudo -S sh -c "echo performance > /sys/devices/system/cpu/cpu0/cpufreq/energy_performance_preference"
-	echo PASSWD | sudo -S sh -c "echo performance > /sys/devices/system/cpu/cpu1/cpufreq/energy_performance_preference"
-	echo PASSWD | sudo -S sh -c "echo performance > /sys/devices/system/cpu/cpu2/cpufreq/energy_performance_preference"
-	echo PASSWD | sudo -S sh -c "echo performance > /sys/devices/system/cpu/cpu3/cpufreq/energy_performance_preference"
+	sudo -S sh -c "echo 0 > /sys/devices/system/cpu/intel_pstate/no_turbo"	
+	sudo -S sh -c "echo performance > /sys/devices/system/cpu/cpu0/cpufreq/energy_performance_preference"
+	sudo -S sh -c "echo performance > /sys/devices/system/cpu/cpu1/cpufreq/energy_performance_preference"
+	sudo -S sh -c "echo performance > /sys/devices/system/cpu/cpu2/cpufreq/energy_performance_preference"
+	sudo -S sh -c "echo performance > /sys/devices/system/cpu/cpu3/cpufreq/energy_performance_preference"
 fi
